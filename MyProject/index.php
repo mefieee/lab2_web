883a195b41b3ee863286ab7d4f296aea348f456e
Vitalii <127862666+mefieee@users.noreply.github.com>
2023-04-12 14:01:45 +0300

    lab2_

--- MyProject/index.php
+++ MyProject/index.php
@@ -3,53 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Header</title>
-    <style>
-        html, body {
-            margin: 0;
-            padding: 0;
-        }
-        body {
-            background-color: whitesmoke;
-        }
-        .header, .main {
-            padding-left: 140px;
-        }
-        header.header {
-            height: 300px;
-        }
-        .header {
-            height: 200px;
-        }
-        @media (min-width: 768px) {
-            .container {
-                position: relative;
-                width: 100%;
-                height: 100%;
-            }
-        }
-        .top-bar {
-            position: absolute;
-            transform: translate(-50%, -50%);
-            top: 100px;
-            left: 30%;
-            -webkit-box-shadow: 0px 40px 38px -8px rgba(156,158,158,0.74);
-            -moz-box-shadow: 0px 40px 38px -8px rgba(156,158,158,0.74);
-            box-shadow: 0px 40px 38px -8px rgba(156,158,158,0.74);
-            width: 200px;
-            height: 88px;
-            background-color: #80b7b3;
-            text-align: center;
-        }
-        .marcy {
-            padding: 15px;
-            margin: 0;
-            font-size: 50px;
-            color: white;
-        }
-        .nav-menu {
-            display: none;
-        }
-    </style>
+    <link rel="stylesheet" href="assets\css\styles.css">
 </head>
 <body>
 <header class="header">
@@ -59,10 +13,5 @@
         </div>
     </div>
 </header>
-<footer class="footer">
-    <div class="container text-center">
-        <small class="copyright"><?= 'Designed by Me!' ?></small>
-    </div><!--//container-->
-</footer><!--//footer-->
 </body>
 </html>
